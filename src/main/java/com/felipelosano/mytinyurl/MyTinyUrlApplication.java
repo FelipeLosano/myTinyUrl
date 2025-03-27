0fef8a9023b7cc3b53f9db1a539b02a58f7bbf34
Felipe Losano <felipelosano@outlook.com>
2025-03-27 15:16:23 -0300

    Initial commit

---
+++ src/main/java/com/felipelosano/mytinyurl/MyTinyUrlApplication.java
@@ -0,0 +1,13 @@
+package com.felipelosano.mytinyurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTinyUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTinyUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipelosano/mytinyurl/MyTinyUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipelosano.mytinyurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTinyUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
